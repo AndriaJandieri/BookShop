40478b3d257648cfbd3805696c3651a8dee85fcc
Andria Jandieri <jandieri.andria@gmail.com>
2025-12-19 17:34:51 +0400

    dropdown visuals change

---
+++ BookShop.Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 10000)]
+
+        public decimal ListPrice { get; set; }
+    }
+}
