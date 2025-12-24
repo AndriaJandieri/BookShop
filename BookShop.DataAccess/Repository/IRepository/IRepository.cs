6244ba553dd2efcbddd4482bb237d56d5bd056cf
Andria Jandieri <jandieri.andria@gmail.com>
2025-12-24 22:11:12 +0400

    update image will delete old one, fill category fieldds in product list

--- BookShop.DataAccess/Repository/IRepository/IRepository.cs
+++ BookShop.DataAccess/Repository/IRepository/IRepository.cs
@@ -11,8 +11,8 @@ namespace BookShop.DataAccess.Repository.IRepository
     {
 
         //T Get(int id);
-        T Get(Expression<Func<T, bool>> filter);
-        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAll(string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
 
         void Add(T entity);
         //void Update(T entity);
--- BookShopWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookShopWeb/Areas/Admin/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace BookShopWeb.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
             return View(objProductList);
         }
         #region Create
