a398b595ed679ebba13dd7dc8efdf35515a2e21a
AndriaJandieri <78377429+AndriaJandieri@users.noreply.github.com>
2026-01-09 05:44:42 +0400

    inttostring

--- BookShop.DataAccess/Migrations/20260109014110_inttostring.Designer.cs
+++ BookShop.DataAccess/Migrations/20260109014110_inttostring.Designer.cs
@@ -4,6 +4,7 @@ using BookShop.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookShop.DataAccess.Migrations
 {
     [DbContext(typeof(BookShopDbContext))]
-    partial class BookShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260109014110_inttostring")]
+    partial class inttostring
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
