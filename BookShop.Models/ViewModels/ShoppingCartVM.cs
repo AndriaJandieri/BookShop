f26654af1fa1719edbb8ea3704e955e91829efde
Andria Jandieri <jandieri.andria@gmail.com>
2026-01-21 15:41:26 +0400

    Shoping cart UI ready

---
+++ BookShop.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        public double CartTotal { get; set; }
+    }
+}
