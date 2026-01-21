45a1bf3a7c541fd21bd76c5cf3effe3ff0223acb
Andria Jandieri <jandieri.andria@gmail.com>
2026-01-21 16:56:59 +0400

    calculating price in shopping cart

--- BookShop.DataAccess/Repository/IRepository/IRepository.cs
+++ BookShop.DataAccess/Repository/IRepository/IRepository.cs
@@ -11,7 +11,7 @@ namespace BookShop.DataAccess.Repository.IRepository
     {
 
         //T Get(int id);
-        IEnumerable<T> GetAll(string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
         T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
 
         void Add(T entity);
--- BookShopWeb/Areas/Customer/Controllers/CartController.cs
+++ BookShopWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BookShop.DataAccess.Repository.IRepository;
+using BookShop.Models;
+using BookShop.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BookShopWeb.Areas.Customer.Controllers
 {
@@ -7,9 +11,48 @@ namespace BookShopWeb.Areas.Customer.Controllers
     [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product"), //Eager loading Product, its keysensitive to spelling, so be careful: use includeProperties exactly as defined Model in DbSet
+                //CartTotal = 0
+            };
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.CartTotal += (cart.Price * cart.Quantity);
+            }
+            return View(ShoppingCartVM);
+
+
+        }
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            if (shoppingCart.Quantity <= 50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else if (shoppingCart.Quantity <= 100)
+            {
+                return shoppingCart.Product.Price50;
+            }
+            else
+            {
+                return shoppingCart.Product.Price100;
+            }
         }
     }
 }
