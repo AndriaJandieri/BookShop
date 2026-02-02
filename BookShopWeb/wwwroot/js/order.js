655677de4bcbdf67593de63e0fd66f023e5a9f98
Andria Jandieri <jandieri.andria@gmail.com>
2026-02-02 19:30:24 +0400

    OrderList UI

--- BookShopWeb/wwwroot/js/order.js
+++ BookShopWeb/wwwroot/js/order.js
@@ -4,19 +4,19 @@
 
 function loadDataTable() {
     dataTable = $('#tableData').DataTable({
-        "ajax": { url: '/admin/product/getall' },
+        "ajax": { url: '/admin/order/getall' },
         "columns": [
-            { data: 'id', "width": "25%" },
+            { data: 'id', "width": "5%" },
             { data: 'name', "width": "15%" },
-            { data: 'phonenumber', "width": "15%" },
-            { data: 'applicationUser.email', "width": "10%" },
-            { data: 'orderstatus', "width": "10%" },
-            { data: 'ordertotal', "width": "10%" },
+            { data: 'phoneNumber', "width": "20%" },
+            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
             {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                        <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2" > <i class="bi bi-pencil-square"></i> Edit</a>
+                        <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2" > <i class="bi bi-pencil-square"></i></a>
                         </div>`
                 },
                 "width": "25%"
