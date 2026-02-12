daf21c05c09c593eb7585ea0ac081d5ec221e30f
Andria Jandieri <jandieri.andria@gmail.com>
2026-02-12 21:02:30 +0400

    Email Sender Implementation started, EMailSender.cs is not ready yet

--- BookShop.Utility/EmailSender.cs
+++ BookShop.Utility/EmailSender.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,18 @@ namespace BookShop.Utility
 {
     public class EmailSender: IEmailSender
     {
+        private readonly string _smtpServer;
+        private readonly int _smtpPort;
+        private readonly string _smtpUser;
+        private readonly string _smtpPass;
+
+        public EmailSender(IConfiguration configuration)
+        { 
+            _smtpServer = configuration["EmailSettings:SmtpServer"];
+            _smtpPort = int.Parse(configuration["EmailSettings:SmtpPort"]);
+            _smtpUser = configuration["EmailSettings:SmtpUser"];
+            _smtpPass = configuration["EmailSettings:SmtpPass"];
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             // Here you would implement your email sending logic.
