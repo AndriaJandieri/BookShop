b13a44c37f791d6c07a73bf75c89746867b08989
Andria Jandieri <jandieri.andria@gmail.com>
2026-02-19 19:01:27 +0400

    repository and unitofwork for productimage

---
+++ BookShop.DataAccess/Repository/ProductImageRepository.cs
@@ -0,0 +1,27 @@
+﻿using BookShop.DataAccess.Data;
+using BookShop.DataAccess.Repository.IRepository;
+using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.DataAccess.Repository
+{
+    public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository
+    {
+
+        private readonly BookShopDbContext _db;
+        public ProductImageRepository(BookShopDbContext db) : base(db)
+        {
+            _db = db;
+        }        
+
+        public void Update(ProductImage obj)
+        {
+            _db.ProductImages.Update(obj);
+        }
+    }
+}
