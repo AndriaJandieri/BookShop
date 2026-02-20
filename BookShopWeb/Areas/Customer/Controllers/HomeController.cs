2024631df2a0662712830b911cb52ca5f52efd9e
Andria Jandieri <jandieri.andria@gmail.com>
2026-02-20 19:57:57 +0400

    product images with bootstrap carousel

--- BookShopWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookShopWeb/Areas/Customer/Controllers/HomeController.cs
@@ -31,14 +31,14 @@ namespace BookShopWeb.Areas.Customer.Controllers
                 HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
             }
 
-            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ProductImages");
             return View(productList);
         }
         public IActionResult Details(int productId)
         {
             ShoppingCart cartObj = new()
             {
-                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category,ProductImages"),
                 Quantity = 1,
                 ProductId = productId
             };
